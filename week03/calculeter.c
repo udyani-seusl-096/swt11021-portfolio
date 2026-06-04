c90d5b4d1ada669e9a9f380cc8c92315b214c1c9
Aloka udyani <aloka.udyani.2003@gmail.com>
2026-06-04 11:38:04 +0530

    week03: operators , bitwise , precedence , calculator

---
+++ week03/calculeter.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ int main (void){
+ printf("\n");
+ printf("\n");
+ printf("*******  MINI CALCULATER *******\n");
+ printf("\n");
+ printf("\n");
+ printf("\n");
+         double num1 , num2;
+
+         printf("Enter the first number: ");
+         scanf("%lf", &num1);
+         
+         printf("Enter the second number: ");
+         scanf("%lf", &num2);
+
+         printf("\n");
+         printf("\n");
+         printf("\n");
+
+         printf("Answer-->\n");
+
+         printf("\n");
+         printf("addition of num1 & num2:             %f\n",num1 + num2);
+         printf("subtraction of num1 & num2:          %f\n",num1 - num2);
+         printf("multiplication of num1 & num2:       %f\n",num1 * num2);
+         printf("division of num1 & num2:             %f\n",num1 / num2);
+
+         printf("\n");
+         printf("\n");
+         printf("\n");
+
+ }
